6dd7caa437447c3aa650d8aa4fd5ce62ee5ef0e1
TheAppleMk12 <Abramhtran@gmail.com>
2024-10-24 11:22:25 -0700

    1 an 2

--- Main.java
+++ Main.java
@@ -5,5 +5,23 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    //Problem 1
+    Scanner sc = new Scanner(System.in);
+    double x = 12.345;
+
+    System.out.println("Please enter a double:");
+    double y = sc.nextDouble();
+
+    if (x==y){
+      System.out.print("YES\n");
+    }
+    int a = 48;
+
+    System.out.println("Please enter an integer:");
+    int b = sc.nextInt();
+
+    if (a==b){
+      System.out.print("YES\n");
+  }
   }
 }
